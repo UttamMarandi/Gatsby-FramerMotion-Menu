f1be735fe5b2a6520976585232ea7cc75d0f6f26
Uttam Marandi <uttammarandi007@gmail.com>
2021-09-08 18:56:24 +0530

    looping and mapping through products.json, understand products.json

--- src/components/menu.js
+++ src/components/menu.js
@@ -4,6 +4,9 @@ import { Close } from "../icons/icons"
 import { Image } from "../components/gatsby-images/image"
 import { Link } from "gatsby"
 
+//data
+import data from "../data/products.json"
+
 function Menu() {
   return (
     <div className="products">
@@ -15,34 +18,72 @@ function Menu() {
         <div className="container">
           <div className="menu-inner">
             <ul>
-              <li>
-                <Link to={`/product/a`}>
-                  <div className="wrapper">
-                    <div className="line left flex-0">
-                      {/* <div className="mask"></div> */}
-                    </div>
-                    <div className="title">
-                      <h2>
-                        <div className="text">Air Max 90</div>
-                      </h2>
-                    </div>
-                    <div className="thumbnail">
-                      <Image src={1} />
-                    </div>
-                    <div className="floating-image">
-                      <Image src={1} />
-                    </div>
-                    <div className="line right flex-1">
-                      {/* <div className="mask right"></div> */}
-                    </div>
-                  </div>
-                </Link>
-              </li>
+              {data.map(
+                (
+                  {
+                    title,
+                    id,
+                    leftLineFlex,
+                    rightLineFlex,
+                    thumbnailPosition,
+                    offset,
+                    src,
+                  },
+                  index
+                ) => (
+                  <List
+                    key={index}
+                    title={title}
+                    id={id}
+                    leftLineFlex={leftLineFlex}
+                    rightLineFlex={rightLineFlex}
+                    thumbnailPosition={thumbnailPosition}
+                    offset={offset}
+                    src={src}
+                  />
+                )
+              )}
             </ul>
           </div>
         </div>
       </div>
     </div>
+  )
+}
+
+const List = ({
+  title,
+  id,
+  leftLineFlex,
+  rightLineFlex,
+  thumbnailPosition,
+  offset,
+  src,
+}) => {
+  return (
+    <li>
+      <Link to={`/product/${id}`}>
+        <div className="wrapper">
+          <div className={`line left flex-${leftLineFlex}`}>
+            {/* <div className="mask"></div> */}
+          </div>
+          <div className="title">
+            <h2>
+              <div className="text">{title}</div>
+            </h2>
+          </div>
+          <div className="thumbnail" style={{ left: thumbnailPosition }}>
+            <Image src={src} />
+          </div>
+          <div className="floating-image">
+            <Image src={src} />
+          </div>
+          <div className={`line right flex-${rightLineFlex}`}>
+            {/* <div className="mask right"></div> */}
+          </div>
+        </div>
+      </Link>
+    </li>
   )
 }
 
